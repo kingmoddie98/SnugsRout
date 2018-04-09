f7e4356f7a3f5706825170a075738850b8dfb5e9
kingmoddie98 <kingmoddie98@gmail.com>
2018-04-08 22:10:15 -0400

    Mouse click crap
    Please god make it stop

---
+++ Snug/Assets/MousePoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePoint : MonoBehaviour {
+
+    RaycastHit hit;
+
+    private float rayCastLength = 500;
+
+     void Update()
+    {
+        GameObject target = GameObject.Find("Target");
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            Debug.Log(hit.collider.name);
+            if (hit.collider.name == "Plane")
+            {
+                target.transform.position = hit.point;
+            }
+
+
+        }
+
+
+        Debug.DrawRay(ray.origin, ray.direction * rayCastLength, Color.yellow);
+
+    }
+}
