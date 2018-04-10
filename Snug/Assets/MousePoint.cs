de3849f67b32a4f2f1a5ca3fa3f3a75989068f02
kingmoddie98 <kingmoddie98@gmail.com>
2018-04-09 23:40:39 -0400

    Mouse pointer
    https://www.youtube.com/watch?v=bE6DQdnfe3U&list=PLcGWkWha2wFd5Azr14ZJjvfnAmUKM_3E7&index=6

--- Snug/Assets/MousePoint.cs
+++ Snug/Assets/MousePoint.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class MousePoint : MonoBehaviour {
 
     RaycastHit hit;
-
+    public GameObject Target;
     private float rayCastLength = 500;
 
      void Update()
     {
-        GameObject target = GameObject.Find("Target");
+        //GameObject target = GameObject.Find("Target");
+      
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit, 1000))
@@ -18,7 +19,17 @@ public class MousePoint : MonoBehaviour {
             Debug.Log(hit.collider.name);
             if (hit.collider.name == "Plane")
             {
-                target.transform.position = hit.point;
+                //target.transform.position = hit.point;
+                //When we right click instantiate object
+                //0 is left mouse 1 right 2 middle
+                if(Input.GetMouseButton(1))
+                {
+                    GameObject TargetObj = Instantiate(Target, hit.point, Quaternion.identity) as GameObject;
+                    TargetObj.name = "Target Instantiated";
+
+                    GameObject.Destroy(TargetObj, 2.0f);
+
+                }
             }
 
 
