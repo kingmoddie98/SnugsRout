2e3c78dde9000086e6436f07215870476698b044
kingmoddie98 <kingmoddie98@gmail.com>
2018-04-10 23:02:33 -0400

    Got pathing working
    yesssssss

--- Snug/Assets/Scripts/MousePoint.cs
+++ Snug/Assets/Scripts/MousePoint.cs
@@ -5,38 +5,130 @@ using UnityEngine;
 public class MousePoint : MonoBehaviour {
 
     RaycastHit hit;
+    public static GameObject CurrentlySelectedUnit;
+
+    public static Vector3 rightClickPoint;
+
     public GameObject Target;
+    
     private float rayCastLength = 500;
 
-     void Update()
+    private static Vector3 mouseDownPoint;
+    
+
+    private void Awake()
+    {
+        mouseDownPoint = Vector3.zero;
+    }
+    void Update()
     {
         //GameObject target = GameObject.Find("Target");
       
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit, 1000))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
+            //store point of mouse button down
+            if (Input.GetMouseButton(0))
+            {
+                mouseDownPoint = hit.point;
+            }
+
             Debug.Log(hit.collider.name);
             if (hit.collider.name == "Plane")
             {
                 //target.transform.position = hit.point;
                 //When we right click instantiate object
                 //0 is left mouse 1 right 2 middle
-                if(Input.GetMouseButton(1))
+                if (Input.GetMouseButton(1))
                 {
                     GameObject TargetObj = Instantiate(Target, hit.point, Quaternion.identity) as GameObject;
                     TargetObj.name = "Target Instantiated";
+                    rightClickPoint = hit.point;
 
                     GameObject.Destroy(TargetObj, 2.0f);
 
                 }
+                else if (Input.GetMouseButtonUp(0) && DidUserClickLeftMouse(mouseDownPoint))
+                {
+                    DeselectGameObjectIfSelected();
+                }
+            }
+            else
+            {
+                if(Input.GetMouseButtonUp(0) && DidUserClickLeftMouse(mouseDownPoint))
+                {
+                    //is user hittign something we can select
+                    if (hit.collider.transform.FindChild("Selected"))
+                    {
+                        Debug.Log("Found a unit");
+
+                        //are we selecting a different object
+                        if(CurrentlySelectedUnit != hit.collider.gameObject)
+                        {
+                            GameObject SelectedObj = hit.collider.transform.FindChild("Selected").gameObject;
+                            SelectedObj.active = true;
+
+                            hit.collider.gameObject.GetComponent<Unit>().Selected = true;
+                            //deactive currently selected object
+                            if(CurrentlySelectedUnit != null)
+                            {
+                                CurrentlySelectedUnit.transform.FindChild("Selected").gameObject.active = false;
+                            }
+
+                            CurrentlySelectedUnit = hit.collider.gameObject;
+
+
+                        }
+                    }
+                    else
+                    {
+                        //if not a selected unit
+                        DeselectGameObjectIfSelected();
+                    }
+                }
             }
 
-
         }
+        else
+        {
+            if (Input.GetMouseButtonUp(0) && DidUserClickLeftMouse(mouseDownPoint))
+            {
+                DeselectGameObjectIfSelected();
+            }
+        }
+    
+
+        
 
 
         Debug.DrawRay(ray.origin, ray.direction * rayCastLength, Color.yellow);
 
     }
+
+    //helper functions
+
+
+    public bool DidUserClickLeftMouse(Vector3 hitPoint)
+    {
+        float clickZone = 1.3f;
+
+        if((mouseDownPoint.x < hitPoint.x + clickZone && mouseDownPoint.x > hitPoint.x - clickZone) &&
+                (mouseDownPoint.y < hitPoint.y + clickZone && mouseDownPoint.y > hitPoint.y - clickZone) &&
+                (mouseDownPoint.z < hitPoint.z + clickZone && mouseDownPoint.z > hitPoint.z- clickZone))
+        {
+            return true; 
+        }
+        return false;
+    }
+
+    public static void DeselectGameObjectIfSelected()
+    {
+        if(CurrentlySelectedUnit != null)
+        {
+            CurrentlySelectedUnit.transform.FindChild("Selected").gameObject.active = false;
+            CurrentlySelectedUnit = null;
+        }
+    }
+
 }
---
+++ Snug/Assets/Scripts/UnitPath.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UnitPath : MonoBehaviour {
+
+    private Unit unit;
+    Vector3[] path;
+    int targetIndex;
+    float speed = 20;
+
+    public void Start()
+    {
+        unit = GetComponent<Unit>();
+    }
+
+    public void LateUpdate()
+    {
+        if (unit.Selected)
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                PathRequestManager.RequestPath(transform.position, MousePoint.rightClickPoint, OnPathFound);
+            }
+        }
+
+
+        
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0];
+        while (true)
+        {
+            if (transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Length)
+                {
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex];
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            for (int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(path[i], Vector3.one);
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
+    }
+}
+
